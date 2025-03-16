d6ff5b79052d997c424eb10eb02e8cf89a7e69a5
Atharv-001 <atharv.coder28@gmail.com>
2025-03-16 13:33:20 +0530

    Add files via upload

---
+++ src/client/java/villager/staffmodv1/AtmodV1Client.java
@@ -0,0 +1,10 @@
+package villager.staffmodv1;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AtmodV1Client implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
